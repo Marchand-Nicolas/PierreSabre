03861aebca101eeb2f3376c0944b6bba404cde90
Nico <60229704+Marchand-Nicolas@users.noreply.github.com>
2025-11-17 16:23:51 +0100

    création de la classe Humain

---
+++ src/personnages/Humain.java
@@ -0,0 +1,52 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private String boisson;
+	private int argent;
+
+	public Humain(String nom, String boisson, int argent) {
+		super();
+		this.nom = nom;
+		this.boisson = boisson;
+		this.argent = argent;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public int getArgent() {
+		return argent;
+	}
+
+	public void direBonjour() {
+		System.out.println("Bonjour ! Je m'appelle " + nom + " et j'aime boire du " + boisson + ".");
+	}
+
+	private void parler(String texte) {
+		System.out.println("L'humain " + nom + " : " + texte);
+	}
+
+	public void boire() {
+		parler("Mmmm un bon verre de " + boisson + " Gloups !");
+	}
+
+	public void acheter(String bien, int prix) {
+		if (prix < argent) {
+			parler("J'ai " + argent + " sous en poche. Je vais pouvoir m'offrir un " + bien + " à " + prix + " sous.");
+			argent -= prix;
+		} else {
+			parler("Je n'ai plus que " + argent + " sous en poche. Je ne peux même pas m'offrir un" + bien + " à "
+					+ prix + " sous.");
+		}
+	}
+
+	public void gagnerArgent(int gain) {
+		argent += gain;
+	}
+
+	public void perdreArgent(int perte) {
+		argent -= perte;
+	}
+}
